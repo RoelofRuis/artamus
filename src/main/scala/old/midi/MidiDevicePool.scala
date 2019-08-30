4c6588868b5f16f965194232afc33b6b2a9a5737
RoelofRuis <roelof.ruis@gmail.com>
2019-08-30 21:05:15 +0200

    removed 2 old files that are not needed

--- src/main/scala/old/midi/MidiDevicePool.scala
+++
@@ -1,9 +0,0 @@
-package old.midi
-
-import javax.inject.Inject
-
-class MidiDevicePool @Inject() (midiDeviceProvider: MidiDeviceProvider) {
-
-  def getInfo: Array[String] = midiDeviceProvider.getInfo
-
-}
