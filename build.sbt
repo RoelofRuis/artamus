aff8d3a4166a575943813fe182fed1b4617e6780
RoelofRuis <roelof.ruis@gmail.com>
2019-09-03 22:35:32 +0200

    Added some logging to the client

--- build.sbt
+++ build.sbt
@@ -5,10 +5,13 @@ version := "0.1"
 scalaVersion := "2.12.8"
 
 val scalaGuiceVersion = "4.2.2"
-val typesafeConfigVersion = "1.3.3"
+val scalaLoggingVersion = "3.9.2"
+val slf4jSimpleVersion = "1.7.28"
 
 libraryDependencies ++= Seq(
-  "net.codingwell" %% "scala-guice" % scalaGuiceVersion
+  "net.codingwell" %% "scala-guice" % scalaGuiceVersion,
+  "com.typesafe.scala-logging" %% "scala-logging" % scalaLoggingVersion,
+  "org.slf4j" % "slf4j-simple" % slf4jSimpleVersion
 )
 
 scalacOptions ++= Seq(
--- src/main/scala/protocol/client/DefaultClient.scala
+++ src/main/scala/protocol/client/DefaultClient.scala
@@ -3,13 +3,14 @@ package protocol.client
 import java.io.{ObjectInputStream, ObjectOutputStream}
 import java.net.{InetAddress, Socket}
 
+import com.typesafe.scalalogging.LazyLogging
 import protocol.ClientInterface.EventListener
 import protocol._
 
 import scala.reflect.ClassTag
 import scala.util.Try
 
-private[protocol] class DefaultClient private[protocol] (port: Int) extends ClientInterface {
+private[protocol] class DefaultClient private[protocol] (port: Int) extends ClientInterface with LazyLogging {
 
   private val socket = new Socket(InetAddress.getByName("localhost"), port)
 
@@ -22,29 +23,35 @@ private[protocol] class DefaultClient private[protocol] (port: Int) extends Clie
   private val out = new ClientOutputStream(objectOut)
 
   def sendControl[A <: Control](message: A): Option[Boolean] = {
+    logger.info(s"Send CONTROL [$message]")
     out.sendControl(message)
     val (response, events) = in.expectResponseMessage[Boolean]
 
     handleEvents(events)
 
+    logger.info(s"Received [$response]")
     response.toOption
   }
 
   def sendCommand[A <: Command](message: A): Option[Boolean] = {
+    logger.info(s"Send COMMAND [$message]")
     out.sendCommand(message)
     val (response, events) = in.expectResponseMessage[Boolean]
 
     handleEvents(events)
 
+    logger.info(s"Received [$response]")
     response.toOption
   }
 
   def sendQuery[A <: Query](message: A): Option[A#Res] = {
+    logger.info(s"Send QUERY [$message]")
     out.sendQuery(message)
     val (response, events) = in.expectResponseMessage[A#Res]
 
     handleEvents(events)
 
+    logger.info(s"Received [$response]")
     response.toOption
   }
 
