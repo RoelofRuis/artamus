5b3b671a7f3ebdcf3f787c58d576f9621c6c0a49
RoelofRuis <roelof.ruis@gmail.com>
2019-09-04 21:24:52 +0200

    2 little ideas

--- src/main/scala/protocol/client/package.scala
+++ src/main/scala/protocol/client/package.scala
@@ -6,12 +6,15 @@ package object client {
 
   trait ClientInterface {
 
+    // TODO: Maybe move these 3 to control bus?
     def sendControl[A <: Control](message: A): Option[Boolean]
 
     def sendCommand[A <: Command](message: A): Option[Boolean]
 
     def sendQuery[A <: Query](message: A): Option[A#Res]
 
+
+    // TODO: Maybe move this to initialization
     def subscribe[A <: Event: ClassTag](callback: A => A#Res): Unit
 
     def closeConnection(): Unit
