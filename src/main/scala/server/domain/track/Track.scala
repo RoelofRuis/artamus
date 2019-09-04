322b1848cf26a6f0374ff8f6524e64949bc74ff8
RoelofRuis <roelof.ruis@gmail.com>
2019-09-04 17:40:53 +0200

    Defined property maps to hold data

--- src/main/scala/server/domain/track/Track.scala
+++ src/main/scala/server/domain/track/Track.scala
@@ -9,20 +9,32 @@ import scala.language.existentials
 // TODO: move to music?
 final class Track private () {
 
-  private var symbols: SortedMap[Position, Map[TrackSymbol[_], Any]] = SortedMap[Position, Map[TrackSymbol[_], Any]]()
-  private var properties: Map[TrackProperty[_], Any] = Map[TrackProperty[_], Any]()
+  private var symbols: SortedMap[Position, PropertyMap[TrackSymbol]] = SortedMap[Position, PropertyMap[TrackSymbol]]()
 
-  def addProperty[A](a: A)(implicit ev: TrackProperty[A]): Unit = properties = properties.updated(ev, a)
+  private var properties: PropertyMap[TrackProperty] = PropertyMap[TrackProperty]()
 
-  def getProperty[A](implicit ev: TrackProperty[A]): Option[A] = properties.get(ev).map(_.asInstanceOf[A])
+  def addProperty[A](a: A)(implicit ev: TrackProperty[A]): Unit = properties = properties.add(a)
+
+  def getProperty[A](implicit ev: TrackProperty[A]): Option[A] = properties.get(ev).headOption
 
   def addSymbol[A](pos: Position, a: A)(implicit ev: TrackSymbol[A]): Unit = {
-    symbols = symbols.updated(pos, symbols.getOrElse(pos, Map[TrackSymbol[_], Any]()).updated(ev, a))
+    symbols = symbols.updated(
+      pos,
+      symbols.getOrElse(pos, PropertyMap[TrackSymbol]()).add(a)
+    )
   }
 
-  def getSymbols[A](implicit ev: TrackSymbol[A]): Iterable[A] = {
-    symbols.values.map(_.get(ev)).collect { case Some(s) => s.asInstanceOf[A] }
-  }
+  def getSymbols[A](implicit ev: TrackSymbol[A]): Iterable[A] = symbols.values.flatMap(_.get(ev))
+
+}
+
+import scala.language.higherKinds
+
+case class PropertyMap[V[_]](map: Map[V[_], List[Any]] = Map[V[_], List[Any]]()) {
+
+  def add[A](a: A)(implicit ev: V[A]): PropertyMap[V] = PropertyMap[V](map.updated(ev, map.getOrElse(ev, List()) :+ a))
+
+  def get[A](implicit ev: V[A]): List[A] = map.getOrElse(ev, List()).map(_.asInstanceOf[A])
 
 }
 
