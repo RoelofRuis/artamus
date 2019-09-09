793e27929bacfe9a57577f8730f8518a72357717
RoelofRuis <roelof.ruis@gmail.com>
2019-09-09 20:08:04 +0200

    Relative accidental to text

--- src/main/scala/lilypond/LilypondTest.scala
+++ src/main/scala/lilypond/LilypondTest.scala
@@ -21,13 +21,14 @@ object LilypondTest extends App {
       case 5 => "a"
       case 6 => "b"
     }
-    val accidentalText = mvec.acc match {
-      case Accidental(0) => ""
-      case Accidental(1) => "is"
-      case Accidental(-1) => if (stepValue == 2 || stepValue == 4) "s" else "es"
-      case _ => "" // TODO: support the recursive case!
+    def accidentalText(a: Accidental, acc: String = "", suppressE: Boolean = false): String = {
+      a match {
+        case Accidental(0) => acc
+        case Accidental(v) if v > 0 => accidentalText(Accidental(v - 1), acc + "is")
+        case Accidental(v) if v < 0 => accidentalText(Accidental(v + 1), acc + (if (suppressE) "s" else "es"))
+      }
     }
-    name + accidentalText
+    name + accidentalText(mvec.acc, suppressE = true)
   }
 
   def durationToLily(duration: Duration): String = s"${duration.value.d}"
@@ -36,7 +37,8 @@ object LilypondTest extends App {
     (Duration.QUARTER, MusicVector(Step(0), Accidental(0))),
     (Duration.QUARTER, MusicVector(Step(2), Accidental(-1))),
     (Duration.QUARTER, MusicVector(Step(3), Accidental(0))),
-    (Duration.QUARTER, MusicVector(Step(3), Accidental(1)))
+    (Duration.QUARTER, MusicVector(Step(3), Accidental(1))),
+    (Duration.QUARTER, MusicVector(Step(3), Accidental(2))),
   )
 
   println(notesToLily(notes))
--- src/main/scala/music/MidiPitch.scala
+++ src/main/scala/music/MidiPitch.scala
@@ -2,12 +2,12 @@ package music
 
 import music.properties.Pitch.MidiPitchNumber
 
-final case class MidiPitch private (pitchClass: PitchClass, octave: Octave) {
+final case class MidiPitch private (octave: Octave, pitchClass: PitchClass) {
   def getMidiPitchNumber: MidiPitchNumber = ((octave.value + 1) * 12) + pitchClass.value
 }
 
 object MidiPitch {
 
-  def apply(i: MidiPitchNumber): MidiPitch = MidiPitch(PitchClass(i % 12), Octave((i / 12) - 1))
+  def apply(i: MidiPitchNumber): MidiPitch = MidiPitch(Octave((i / 12) - 1), PitchClass(i % 12))
 
 }
