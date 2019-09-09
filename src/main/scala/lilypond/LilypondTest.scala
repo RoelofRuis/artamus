f080dd8b3da0eeb38578777e17a416db822ef342
RoelofRuis <roelof.ruis@gmail.com>
2019-09-09 19:15:39 +0200

    Outlined how lilypond should work on MusicVector

--- src/main/scala/lilypond/LilypondTest.scala
+++ src/main/scala/lilypond/LilypondTest.scala
@@ -4,32 +4,39 @@ import music._
 
 object LilypondTest extends App {
 
-  def notesToLily(notes: List[Note]): String = {
-    notes.map { note =>
-      pitchToLily(note.midiPitch) + durationToLily(note.duration)
+  def notesToLily(notes: List[(Duration, MusicVector)]): String = {
+    notes.map { case (duration, mvec) =>
+      musicVectorToLily(mvec) + durationToLily(duration)
     }.mkString(" ")
   }
 
-  def pitchToLily(midiPitch: MidiPitch): String = {
-    Scale.MAJOR_SCALE_MATH.pitchClassToStep(midiPitch.pitchClass) match {
-      case Some(Step(0)) => "c"
-      case Some(Step(1)) => "d"
-      case Some(Step(2)) => "e"
-      case Some(Step(3)) => "f"
-      case Some(Step(4)) => "g"
-      case Some(Step(5)) => "a"
-      case Some(Step(6)) => "b"
-      case _ => "r"
+  def musicVectorToLily(mvec: MusicVector): String = {
+    val stepValue = mvec.step.value
+    val name = stepValue match {
+      case 0 => "c"
+      case 1 => "d"
+      case 2 => "e"
+      case 3 => "f"
+      case 4 => "g"
+      case 5 => "a"
+      case 6 => "b"
     }
+    val accidentalText = mvec.acc match {
+      case Accidental(0) => ""
+      case Accidental(1) => "is"
+      case Accidental(-1) => if (stepValue == 2 || stepValue == 4) "s" else "es"
+      case _ => "" // TODO: support the recursive case!
+    }
+    name + accidentalText
   }
 
-  def durationToLily(duration: Duration): String = s"${duration.value.n}"
+  def durationToLily(duration: Duration): String = s"${duration.value.d}"
 
-  val notes: List[Note] = List(
-    Note(Duration.QUARTER, MidiPitch(64)),
-    Note(Duration.QUARTER, MidiPitch(66)),
-    Note(Duration.QUARTER, MidiPitch(68)),
-    Note(Duration.QUARTER, MidiPitch(69))
+  val notes: List[(Duration, MusicVector)] = List(
+    (Duration.QUARTER, MusicVector(Step(0), Accidental(0))),
+    (Duration.QUARTER, MusicVector(Step(2), Accidental(-1))),
+    (Duration.QUARTER, MusicVector(Step(3), Accidental(0))),
+    (Duration.QUARTER, MusicVector(Step(3), Accidental(1)))
   )
 
   println(notesToLily(notes))
