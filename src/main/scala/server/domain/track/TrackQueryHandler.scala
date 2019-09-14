b0c32c2777b2afd4d00da031728a7e0983ead038
RoelofRuis <roelof.ruis@gmail.com>
2019-09-14 19:42:30 +0200

    Crude writing of note sequences

--- src/main/scala/server/domain/track/TrackQueryHandler.scala
+++ src/main/scala/server/domain/track/TrackQueryHandler.scala
@@ -1,7 +1,6 @@
 package server.domain.track
 
 import javax.inject.Inject
-import music.{MidiPitch, Note, Position}
 import protocol.Query
 import pubsub.Dispatcher
 
@@ -14,9 +13,9 @@ private[server] class TrackQueryHandler @Inject() (
   import music.properties.Symbols._
 
   dispatcher.subscribe[GetMidiPitches.type]{ _ =>
-    state.getTrack.getStackedSymbolsAt[Note[MidiPitch]](Position.zero)
-      .map(note => midiPitchHasExactPitch.getMidiNoteNumber(note.pitch).value)
-      .toList
+    state.getTrack.getAllStackedSymbols.map {
+      case (_, notes) => midiPitchHasExactPitch.getMidiNoteNumber(notes.head.pitch).value
+    }.toList
   }
 
 }
--- src/main/scala/server/view/TrackView.scala
+++ src/main/scala/server/view/TrackView.scala
@@ -3,9 +3,9 @@ package server.view
 import java.io._
 
 import javax.inject.Inject
-import music.{MidiPitch, Note, Position}
 import music.interpret.NaivePitchSpelling
 import music.write.LilypondFormatDummy
+import music.{MidiPitch, Note}
 import protocol.Event
 import pubsub.EventBus
 import server.domain.track.{TrackState, TrackSymbolsUpdated}
@@ -19,7 +19,9 @@ class TrackView @Inject() (
     case TrackSymbolsUpdated =>
       val currentState = trackState.getTrack
 
-      val notes = currentState.getStackedSymbolsAt[Note[MidiPitch]](Position.zero)
+      val notes = currentState
+        .getAllStackedSymbols[Note[MidiPitch]]
+        .map { case (_, stackedNotes) => stackedNotes.head }
       val scientificPitch = NaivePitchSpelling.interpret(notes.map(_.pitch))
 
       val spelledNotes = notes.zip(scientificPitch).map { case (note, sp) => Note(note.duration, sp) }
