6915eee13cb66318b6336900df8abf2c8211ed16
RoelofRuis <roelof.ruis@gmail.com>
2019-09-16 00:48:27 +0200

    Correcting the intervals

--- src/main/scala/music/symbolic/const/Intervals.scala
+++ src/main/scala/music/symbolic/const/Intervals.scala
@@ -5,10 +5,10 @@ import music.symbolic.{Accidental, Interval, MusicVector, Step}
 
 object Intervals {
 
-
   private def build(step: Int, acc: Accidental): Interval = Interval(MusicVector(Step(step), acc))
 
   lazy val ROOT: Interval = build(0, NEUTRAL)
+  lazy val FLAT_TWO: Interval = build(1, FLAT)
   lazy val TWO: Interval = build(1, NEUTRAL)
   lazy val FLAT_THREE: Interval = build(2, FLAT)
   lazy val THREE: Interval = build(2, NEUTRAL)
@@ -17,11 +17,21 @@ object Intervals {
   lazy val FLAT_FIVE: Interval = build(4, FLAT)
   lazy val FIVE: Interval = build(4, NEUTRAL)
   lazy val SHARP_FIVE: Interval = build(4, SHARP)
-  lazy val DIM_SIX: Interval = build(5, DOUBLE_FLAT)
   lazy val FLAT_SIX: Interval = build(5, FLAT)
   lazy val SIX: Interval = build(5, NEUTRAL)
   lazy val SHARP_SIX: Interval = build(5, SHARP)
+  lazy val DIM_SEVEN: Interval = build(6, DOUBLE_FLAT)
   lazy val FLAT_SEVEN: Interval = build(6, FLAT)
   lazy val SEVEN: Interval = build(6, NEUTRAL)
+
+  lazy val NAMED_FUNCTIONS: Seq[Interval] = Seq(
+    ROOT,
+    FLAT_TWO, TWO,
+    FLAT_THREE, THREE,
+    FOUR, SHARP_FOUR,
+    FLAT_FIVE, FIVE, SHARP_FIVE,
+    FLAT_SIX, SIX, SHARP_SIX,
+    DIM_SEVEN, FLAT_SEVEN, SEVEN
+  )
 
 }
