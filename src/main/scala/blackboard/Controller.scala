d34f6c18deb59c83458b935f54e7a75f1b81b6d9
RoelofRuis <roelof.ruis@gmail.com>
2019-10-07 23:36:38 +0200

    Removed obtrusive logging

--- src/main/scala/blackboard/Controller.scala
+++ src/main/scala/blackboard/Controller.scala
@@ -1,14 +1,11 @@
 package blackboard
 
-import com.typesafe.scalalogging.LazyLogging
-
-class Controller[A](val knowledgeSources: Seq[KnowledgeSource[A]]) extends LazyLogging {
+class Controller[A](val knowledgeSources: Seq[KnowledgeSource[A]]) {
 
   def run(state: A): A = {
     knowledgeSources.foldLeft(state) {
       case (s, ks) =>
         val newState = ks.execute(s)
-        logger.debug("New state {}", newState)
         newState
     }
   }
