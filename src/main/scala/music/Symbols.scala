94079ccd1c81198c7ac059b277fede334b82aae3
RoelofRuis <roelof.ruis@gmail.com>
2019-10-10 18:35:28 +0200

    Better protection for symbols

--- src/main/scala/music/Symbols.scala
+++ src/main/scala/music/Symbols.scala
@@ -4,20 +4,24 @@ import music.primitives._
 
 object Symbols {
 
-  trait SymbolType
-  trait Property[S <: SymbolType, P]
+  sealed trait SymbolType
+
+  final case class Property[S <: SymbolType, P] private ()
+  object Property {
+    def apply[S <: SymbolType, P]: Property[S, P] = new Property[S, P]()
+  }
 
   case object Note extends SymbolType
-  implicit val noteHasPitchClass: Property[Note.type, PitchClass] = new Property[Note.type, PitchClass] {}
-  implicit val noteHasOctave: Property[Note.type, Octave] = new Property[Note.type, Octave] {}
-  implicit val noteHasDuration: Property[Note.type, Duration] = new Property[Note.type, Duration] {}
+  implicit val noteHasPitchClass: Property[Note.type, PitchClass] = Property[Note.type, PitchClass]
+  implicit val noteHasOctave: Property[Note.type, Octave] = Property[Note.type, Octave]
+  implicit val noteHasDuration: Property[Note.type, Duration] = Property[Note.type, Duration]
 
   case object Chord extends SymbolType
-  implicit val chordHasRoot: Property[Chord.type, ChordRoot] = new Property[Chord.type, ChordRoot] {}
-  implicit val chordHasFunctions: Property[Chord.type, ChordFunctions] = new Property[Chord.type, ChordFunctions] {}
+  implicit val chordHasRoot: Property[Chord.type, ChordRoot] = Property[Chord.type, ChordRoot]
+  implicit val chordHasFunctions: Property[Chord.type, ChordFunctions] = Property[Chord.type, ChordFunctions]
 
   case object MetaSymbol extends SymbolType
-  implicit val metaSymbolHasTimeSignature: Property[MetaSymbol.type, TimeSignature] = new Property[MetaSymbol.type, TimeSignature] {}
-  implicit val metaSymbolHasKey: Property[MetaSymbol.type, Key] = new Property[MetaSymbol.type, Key] {}
+  implicit val metaSymbolHasTimeSignature: Property[MetaSymbol.type, TimeSignature] = Property[MetaSymbol.type, TimeSignature]
+  implicit val metaSymbolHasKey: Property[MetaSymbol.type, Key] = Property[MetaSymbol.type, Key]
 
 }
