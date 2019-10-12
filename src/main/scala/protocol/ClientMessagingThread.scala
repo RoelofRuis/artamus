dbdba47269c647253dd566fa9f8d21410181a4fc
RoelofRuis <roelof.ruis@gmail.com>
2019-10-12 18:33:48 +0200

    ClientMessagingThread contains send and receive for client

--- src/main/scala/protocol/ClientMessagingThread.scala
+++ src/main/scala/protocol/ClientMessagingThread.scala
@@ -7,22 +7,43 @@ import com.typesafe.scalalogging.LazyLogging
 import resource.Resource
 import transport.client.ClientThread
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
-class ReceiveThread(
-  client: Resource[ClientThread],
-  eventQueue: BlockingQueue[Event]
+class ClientMessagingThread(
+  eventQueue: BlockingQueue[Event],
+  client: Resource[ClientThread]
 ) extends Thread with LazyLogging {
 
   private val queue: SynchronousQueue[DataResponse] = new SynchronousQueue[DataResponse]()
   private val expectsData: AtomicBoolean = new AtomicBoolean(false)
 
-  def expect(): Unit = expectsData.set(true)
+  def sendRequest[A, R](request: A): Option[R] = {
+    client.acquire match {
+      case Right(c) =>
+        expectsData.set(true)
+        c.send(request)
+        val response = queue.take()
+        expectsData.set(false)
+        response.data match {
+          case Left(serverError) =>
+            logger.warn(s"server error [$serverError]")
+            None
 
-  def take(): DataResponse = {
-    val res = queue.take()
-    expectsData.set(false)
-    res
+          case Right(data) =>
+            decode[R](data) match {
+              case Success(obj) =>
+                Some(obj)
+
+              case Failure(ex) =>
+                logger.warn(s"Unable to decode message", ex)
+                None
+            }
+        }
+
+      case Left(ex) =>
+        logger.error("Client encountered connection exception", ex)
+        None
+    }
   }
 
   override def run(): Unit = {
@@ -46,6 +67,7 @@ class ReceiveThread(
       }
     } catch {
       case _: InterruptedException =>
+        client.close
         Thread.currentThread().interrupt()
     }
   }
--- src/main/scala/protocol/DefaultClient.scala
+++ src/main/scala/protocol/DefaultClient.scala
@@ -7,8 +7,6 @@ import pubsub.{DispatchThread, Dispatcher}
 import resource.Resource
 import transport.client.ClientThread
 
-import scala.util.{Failure, Success, Try}
-
 class DefaultClient(
   client: Resource[ClientThread],
   eventDispatcher: Dispatcher[Event],
@@ -17,58 +15,25 @@ class DefaultClient(
   private val eventQueue: BlockingQueue[Event] = new ArrayBlockingQueue[Event](64)
 
   private val eventThread: Thread = new DispatchThread[Event](eventQueue, eventDispatcher)
-  private val receiveThread: ReceiveThread = new ReceiveThread(client, eventQueue)
+  private val clientThread: ClientMessagingThread = new ClientMessagingThread(eventQueue, client)
 
   override def open(): Unit = {
     eventThread.start()
-    receiveThread.start()
+    clientThread.start()
   }
 
   override def sendCommand[A <: protocol.Command](message: A): Option[protocol.Command#Res] = {
-    sendRequest[CommandRequest, A#Res](CommandRequest(message))
+    clientThread.sendRequest[CommandRequest, A#Res](CommandRequest(message))
   }
 
   override def sendQuery[A <: protocol.Query](message: A): Option[A#Res] = {
-    sendRequest[QueryRequest, A#Res](QueryRequest(message))
+    clientThread.sendRequest[QueryRequest, A#Res](QueryRequest(message))
   }
 
   override def close(): Unit = {
     eventThread.interrupt()
-    receiveThread.interrupt()
-    client.close
+    clientThread.interrupt()
   }
-
-  private def sendRequest[A, R](request: A): Option[R] = {
-    client.acquire match {
-      case Right(c) =>
-        logger.debug(s"Send request [$request]")
-        // TODO: push this logic to the receiver thread!
-        receiveThread.expect()
-        c.send(request)
-        val response: DataResponse = receiveThread.take()
-        logger.debug(s"Received response [$response]")
-        response.data match {
-          case Left(serverError) =>
-            logger.warn(s"server error [$serverError]")
-            None
-
-          case Right(data) =>
-            decode[R](data) match {
-              case Success(obj) =>
-                Some(obj)
-              case Failure(ex) =>
-                logger.warn(s"Unable to decode message", ex)
-                None
-            }
-        }
-
-      case Left(ex) =>
-        logger.error("Client encountered connection exception", ex)
-        None
-    }
-  }
-
-  private def decode[A](in: Any): Try[A] = Try(in.asInstanceOf[A])
 
 }
 
