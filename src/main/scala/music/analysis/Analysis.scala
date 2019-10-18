592068573ec5b1dfe83fc9fee54fc29e67e1fd93
RoelofRuis <roelof.ruis@gmail.com>
2019-10-18 23:18:58 +0200

    Little reordering

--- src/main/scala/music/analysis/Analysis.scala
+++ src/main/scala/music/analysis/Analysis.scala
@@ -4,28 +4,28 @@ import music.primitives._
 
 object Analysis {
 
-  trait DefinedFunctions[A] {
-    val intervalFunctionMapping: PartialFunction[Interval, Set[Function]]
-  }
-
   trait DefinedIntervals[A] {
     val intervals: Set[Interval]
+  }
+
+  trait DefinedFunctions[A] {
+    val intervalFunctionMapping: PartialFunction[Interval, Set[Function]]
   }
 
   trait DefinedChords[A] {
     val functionChordMapping: Seq[(Set[Function], String)]
   }
 
-  implicit class FunctionAnalysisOps[A : DefinedFunctions](a: TuningSystem[A]) {
-    def possibleFunctions(i: Interval): Set[Function] = {
-      implicitly[DefinedFunctions[A]].intervalFunctionMapping.applyOrElse(i, (_: Interval) => Set[Function]())
-    }
-  }
-
   implicit class IntervalAnalysisOps[A : DefinedIntervals](a: TuningSystem[A]) {
     def possibleIntervals(pc1: PitchClass, pc2: PitchClass): Set[Interval] = {
       val diff = a.pcDiff(pc1, pc2)
       implicitly[DefinedIntervals[A]].intervals.filter(_.pc.value == diff)
+    }
+  }
+
+  implicit class FunctionAnalysisOps[A : DefinedFunctions](a: TuningSystem[A]) {
+    def possibleFunctions(i: Interval): Set[Function] = {
+      implicitly[DefinedFunctions[A]].intervalFunctionMapping.applyOrElse(i, (_: Interval) => Set[Function]())
     }
   }
 
