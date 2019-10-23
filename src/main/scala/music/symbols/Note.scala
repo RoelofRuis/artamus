e109091fd1c184a11980dd7657bf2821010180f9
RoelofRuis <roelof.ruis@gmail.com>
2019-10-23 17:49:48 +0200

    Added scientific pitch

--- src/main/scala/music/symbols/Note.scala
+++ src/main/scala/music/symbols/Note.scala
@@ -1,9 +1,24 @@
 package music.symbols
 
-import music.primitives.{Duration, Octave, PitchClass}
+import music.primitives.{Duration, Octave, PitchClass, ScientificPitch}
 
 final case class Note(
   octave: Octave,
   pitchClass: PitchClass,
-  duration: Duration
-) extends SymbolType
+  duration: Duration,
+  scientificPitch: Option[ScientificPitch]
+) extends SymbolType {
+
+  def withScientificPitch(pitch: ScientificPitch): Note = this.copy(scientificPitch = Some(pitch))
+
+}
+
+object Note {
+
+  def apply(
+    octave: Octave,
+    pitchClass: PitchClass,
+    duration: Duration
+  ): Note = Note(octave, pitchClass, duration, None)
+
+}
