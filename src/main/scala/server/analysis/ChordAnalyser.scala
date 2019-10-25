5e69d69643e3142f5000bfed36ded92bf453d6d3
RoelofRuis <roelof.ruis@gmail.com>
2019-10-25 23:46:04 +0200

    Cleaned up ChordAnalyser

--- src/main/scala/server/analysis/ChordAnalyser.scala
+++ src/main/scala/server/analysis/ChordAnalyser.scala
@@ -1,8 +1,9 @@
 package server.analysis
 
 import music.analysis.TwelveToneChordAnalysis
+import music.symbol.collection.SymbolTrack.Updater
 import music.symbol.collection.Track
-import music.symbol.{Chord, Note}
+import music.symbol.{Chord, Note, collection}
 import server.analysis.blackboard.KnowledgeSource
 
 class ChordAnalyser extends KnowledgeSource[Track] {
@@ -10,25 +11,24 @@ class ChordAnalyser extends KnowledgeSource[Track] {
   override def canExecute(state: Track): Boolean = true
 
   override def execute(track: Track): Track = {
-    val possibleChords = track.getSymbolTrack[Note].readAllGrouped.map { notes =>
+    val possibleChords = track.getSymbolTrack[Note].readAllGrouped.flatMap { notes =>
       val pitches = notes.map { note => note.symbol.pitchClass }
       val dur = notes.map { note => note.symbol.duration }.max
       val possibleChords = TwelveToneChordAnalysis.findChords(pitches).map(_.withDuration(dur))
-      (notes.head.position, possibleChords)
+
+      if (possibleChords.nonEmpty) Some((notes.head.position), possibleChords.head) // TODO: determine best option instead of picking head
+      else None
     }
 
-    possibleChords
-      .groupBy { case (pos, _) => pos }
-      .map { case (_, chords) => chords.head }
-      .foldLeft(track) { case (acc, (pos, chord)) =>
-        chord match {
-          case c if c.nonEmpty => acc
-            .updateSymbolTrack[Chord](
-              _.addSymbolAt(pos, c.head) // TODO: determine best option instead of picking head
-            )
-          case _ => acc
+    val updateChords = new Updater[Chord] {
+      override def apply(symbolTrack: collection.SymbolTrack[Chord]): collection.SymbolTrack[Chord] = {
+        possibleChords.foldLeft(symbolTrack) {
+          case (acc, (pos, chord)) => acc.addSymbolAt(pos, chord)
         }
       }
+    }
+
+    track.updateSymbolTrack(updateChords)
   }
 
 }
