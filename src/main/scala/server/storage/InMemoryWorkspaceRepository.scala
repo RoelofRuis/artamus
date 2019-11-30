5290bdedda75dd470221a847af259af83e1998db
RoelofRuis <roelof.ruis@gmail.com>
2019-11-30 22:15:29 +0100

    Moving a function

--- src/main/scala/server/storage/InMemoryWorkspaceRepository.scala
+++ src/main/scala/server/storage/InMemoryWorkspaceRepository.scala
@@ -14,16 +14,16 @@ class InMemoryWorkspaceRepository @Inject() extends WorkspaceRepository {
   private val workspaceLock = new Object()
   @GuardedBy("workspaceLock") private var workspaces: Map[UserId, Workspace] = Map()
 
+  def put(workspace: Workspace): Try[Unit] = workspaceLock.synchronized {
+    workspaces = workspaces.updated(workspace.owner, workspace)
+    Success(())
+  }
+
   def getByOwner(user: User): Try[Workspace] = workspaceLock.synchronized {
     workspaces.get(user.id) match {
       case Some(workspace) => Success(workspace)
       case None => Failure(EntityNotFoundException("Workspace"))
     }
-  }
-
-  def put(workspace: Workspace): Try[Unit] = workspaceLock.synchronized {
-    workspaces = workspaces.updated(workspace.owner, workspace)
-    Success(())
   }
 
 }
