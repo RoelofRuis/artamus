8f735d79969dd050d75168d3e17c3a9be83f22fa
RoelofRuis <roelof.ruis@gmail.com>
2019-12-18 23:36:04 +0100

    Some work on the recording model

--- src/main/scala/music/model/record/MidiRecording.scala
+++
@@ -1,8 +0,0 @@
-package music.model.record
-
-import music.primitives.TickResolution
-
-final case class MidiRecording(
-  resolution: TickResolution,
-  notes: Seq[RawMidiNote]
-)
---
+++ src/main/scala/music/model/record/Recording.scala
@@ -0,0 +1,19 @@
+package music.model.record
+
+import java.util.UUID
+
+import music.model.record.Recording.RecordingId
+import music.primitives.TickResolution
+
+final case class Recording(
+  resolution: TickResolution,
+  recordingId: RecordingId = RecordingId(),
+  mode: RecordingMode = InputOnly,
+  notes: Seq[RawMidiNote] = Seq()
+)
+
+object Recording {
+
+  final case class RecordingId(id: UUID = UUID.randomUUID())
+
+}
