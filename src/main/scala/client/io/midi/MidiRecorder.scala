6bf39a0ef85fe1513d8d3d585525d016724e0d6e
RoelofRuis <roelof.ruis@gmail.com>
2020-01-13 22:50:29 +0100

    Record notes directly to the server

--- src/main/scala/client/io/midi/MidiRecorder.scala
+++ src/main/scala/client/io/midi/MidiRecorder.scala
@@ -2,20 +2,40 @@ package client.io.midi
 
 import java.util.concurrent.{BlockingQueue, LinkedBlockingQueue}
 
-import javax.sound.midi.{MidiMessage, Receiver}
+import com.typesafe.scalalogging.LazyLogging
+import javax.inject.Inject
+import javax.sound.midi.{MidiMessage, Receiver, ShortMessage}
+import music.model.record.RawMidiNote
+import music.primitives.{Loudness, MidiNoteNumber, MillisecondPosition}
+import protocol.client.api.ClientInterface
+import server.actions.recording.RecordNote
 
-class MidiRecorder extends Thread with Receiver {
+class MidiRecorder @Inject() (
+  client: ClientInterface
+) extends Thread with Receiver with LazyLogging {
 
   private val queue: BlockingQueue[(MidiMessage, Long)] = new LinkedBlockingQueue[(MidiMessage, Long)]()
 
   override def run(): Unit = {
     try {
       while ( ! isInterrupted ) {
-        val elem = queue.take()
-        println(s"Recorded [$elem]")
+        val (message, timestamp) = queue.take()
+        message match {
+          case msg: ShortMessage if msg.getCommand == ShortMessage.NOTE_ON =>
+            val note = RawMidiNote(
+              MidiNoteNumber(msg.getData1),
+              Loudness(msg.getData2),
+              MillisecondPosition(timestamp)
+            )
+            client.sendCommand(RecordNote(note)) match {
+              case None =>
+              case Some(ex) => logger.error("Unable to send recorded note", ex)
+            }
+          case _ =>
+        }
       }
     } catch {
-      case _: InterruptedException =>
+      case _: InterruptedException => logger.info("Midi Recorder was interrupted")
     }
   }
 
