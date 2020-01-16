50a1788adec1ead7dc0c675a641fa9431ed7b255
RoelofRuis <roelof.ruis@gmail.com>
2020-01-16 22:18:13 +0100

    Toward proper quantization, still terrible now

--- src/main/scala/music/analysis/Quantization.scala
+++ src/main/scala/music/analysis/Quantization.scala
@@ -1,6 +1,6 @@
 package music.analysis
 
-import music.math.HierarchicalClustering.Centroid
+import music.math.HierarchicalClustering.{Centroid, Clusters}
 import music.math.temporal.{Duration, Position}
 import music.math.{HierarchicalClustering, Rational}
 
@@ -13,12 +13,10 @@ object Quantization {
   def millisToPosition(input: Seq[Int]): List[Position] = {
     if (input.isEmpty) List()
     else if (input.size == 1) List(Position.ZERO)
-    else if (input.size == 2) List(Position.ZERO, Position(Rational(1, 4)))
     else determinePositions(input)
   }
 
   private def determinePositions(input: Seq[Int]): List[Position] = {
-    // Preprocess data
     val inputToDouble = input.map(_.toDouble)
     val differenceList = (inputToDouble drop 1)
       .lazyZip(inputToDouble)
@@ -27,16 +25,7 @@ object Quantization {
     val clusterSettings = HierarchicalClustering.Settings(distanceThreshold = 100)
     val clusters = HierarchicalClustering.cluster(differenceList, clusterSettings)
 
-    // TODO: cluster to duration determination has to be expanded!
-    val bpm = 120D
-    val quarterDuration = (60 / bpm) * 1000
-    val quarterCentroid = clusters.zipWithIndex.minBy { case (c, _) => Math.abs(c - quarterDuration) }._1
-    val eightCentroid = clusters.zipWithIndex.minBy { case (c, _) => Math.abs(c - (quarterCentroid / 2)) }._1
-
-    val centroidMap = Map[Centroid, Duration](
-      quarterCentroid -> Duration(Rational(1, 4)),
-      eightCentroid -> Duration(Rational(1, 8))
-    )
+    val centroidMap = determineCentroidMap(clusters)
 
     @tailrec
     def pickDistances(data: Seq[Double], acc: List[Position]): List[Position] = {
@@ -49,6 +38,26 @@ object Quantization {
       }
     }
     pickDistances(differenceList, List(Position.ZERO))
+  }
+
+  // TODO: improve this still terrible algorithm!
+  private def determineCentroidMap(clusters: Seq[Centroid]): Map[Centroid, Duration] = {
+    def closestTo(x: Double): Centroid = clusters.minBy(v => Math.abs(v - x))
+
+    val quarterCentroid = closestTo(500) // just assumption! (quarter note based on 120BPM)
+    if (clusters.sorted.indexOf(quarterCentroid) == 0) {
+      val halfCentroid = closestTo(quarterCentroid * 2)
+      Map(
+        quarterCentroid -> Duration(Rational(1, 4)),
+        halfCentroid -> Duration(Rational(1, 2))
+      )
+    } else {
+      val eightCentroid = closestTo(quarterCentroid / 2)
+      Map(
+        eightCentroid -> Duration(Rational(1, 8)),
+        quarterCentroid -> Duration(Rational(1, 4)),
+      )
+    }
   }
 
 }
