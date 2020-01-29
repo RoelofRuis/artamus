900aab2a44a882cbcb8216037b2268b77a38b558
RoelofRuis <roelof.ruis@gmail.com>
2020-01-29 19:02:46 +0100

    Pick just one db implementation

--- server/src/main/scala/server/ServerModule.scala
+++ server/src/main/scala/server/ServerModule.scala
@@ -12,7 +12,7 @@ import storage.api.Database
 class ServerModule extends ScalaPrivateModule with ServerSettings {
 
   override def configure(): Unit = {
-    bind[Database].toInstance(storage.fileDatabase(fileDatabaseConfig))
+//    bind[Database].toInstance(storage.fileDatabase(fileDatabaseConfig))
     bind[Database].toInstance(storage.inMemoryDatabase())
 
     install(new ServerInfraModule)
