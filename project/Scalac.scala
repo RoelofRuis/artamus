bcb8014bccd61c81ed76b2fba5a4eed79b36e609
RoelofRuis <roelof.ruis@gmail.com>
2020-01-30 17:46:05 +0100

    Added some more compiler flags

--- project/Scalac.scala
+++ project/Scalac.scala
@@ -3,12 +3,22 @@ import sbt.Keys.scalacOptions
 object Scalac {
   val compilerFlags = Seq(
     scalacOptions ++= Seq(
-      "-encoding", "UTF-8",
-      "-unchecked",
       "-deprecation",
-      "-Ywarn-dead-code",
+      "-encoding", "UTF-8",
+      "-explaintypes",
       "-feature",
       "-language:implicitConversions",
+      "-unchecked",
+      "-Xlint:infer-any",
+      "-Xlint:type-parameter-shadow",
+      "-Xlint:unsound-match",
+      "-Ywarn-unused:imports",
+      "-Ywarn-unused:locals",
+      "-Ywarn-unused:params",
+      "-Ywarn-unused:patvars",
+      "-Ywarn-unused:privates",
+      "-Ywarn-dead-code",
+      "-Yno-imports",
     )
   )
 }
