b35a5f388bee9ea1eab0a106a5ac548290c611ec
RoelofRuis <roelof.ruis@gmail.com>
2020-04-20 21:32:54 +0200

    cleaned up 'Notes'

--- common/src/main/scala/domain/write/Notes.scala
+++ common/src/main/scala/domain/write/Notes.scala
@@ -11,16 +11,10 @@ final case class Notes private ( // TODO: see if still required
 
   def readGroupsList(): List[NoteGroup] = notes.values.toList
   def readGroups(): Iterator[NoteGroup] = notes.valuesIterator
-  def read(): Iterator[Note] = notes.valuesIterator.flatMap(_.notes).buffered
+  def read(): Iterator[Note] = notes.valuesIterator.flatMap(_.notes)
 
   def writeNoteGroup(noteGroup: NoteGroup): Notes = copy(
       notes = notes.updated(noteGroup.window.start, noteGroup)
-    )
-
-  def mapNotes(f: Note => Note): Notes = copy(
-      notes = notes.map {
-        case (position, noteGroup) => (position, NoteGroup(noteGroup.window, noteGroup.notes.map(f)))
-      }
     )
 
 }
