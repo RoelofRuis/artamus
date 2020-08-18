597f393064d56418508caf249493ab215a0db4f0
RoelofRuis <roelof.ruis@gmail.com>
2020-08-18 09:55:45 +0200

    Correct spelling based on minimum distance pitch class difference

--- applications/src/main/scala/nl/roelofruis/artamus/degree/Printing.scala
+++ applications/src/main/scala/nl/roelofruis/artamus/degree/Printing.scala
@@ -13,7 +13,14 @@ object Printing {
 
     def printPitchDescriptor(descriptor: PitchDescriptor): String = {
       val base = tuning.noteNames(descriptor.step)
-      val diff = descriptor.pitchClass - tuning.pitchClassSequence(descriptor.step)
+      val diff = Seq(
+        descriptor.pitchClass,
+        descriptor.pitchClass + tuning.numPitchClasses,
+        descriptor.pitchClass - tuning.numPitchClasses
+      )
+        .map(_ - tuning.pitchClassSequence(descriptor.step))
+        .minBy(Math.abs)
+
       if (diff > 0) base + (tuning.textSharp * diff)
       else if (diff < 0) base + (tuning.textFlat * -diff)
       else base
