8b6ef1870bab9e147155550e8d8ec09e88fb0d9a
RoelofRuis <roelof.ruis@gmail.com>
2020-08-22 12:35:49 +0200

    some debug printing

--- applications/src/main/scala/nl/roelofruis/artamus/analysis/RNA.scala
+++ applications/src/main/scala/nl/roelofruis/artamus/analysis/RNA.scala
@@ -5,7 +5,7 @@ import nl.roelofruis.artamus.degree.Model.{Chord, Degree, Key, PitchDescriptor}
 
 case class RNA(tuning: TextTuning) extends TuningMaths {
 
-  trait DegreeHypothesis
+  sealed trait DegreeHypothesis
 
   final case object UnknownDegree extends DegreeHypothesis
   final case class DegreeInKey(
@@ -14,13 +14,18 @@ case class RNA(tuning: TextTuning) extends TuningMaths {
   ) extends DegreeHypothesis
 
   def nameDegrees(chords: Seq[Chord], baseKey: Key): Seq[Degree] = {
+    import nl.roelofruis.artamus.degree.Write._ // TODO: remove this
+
     // Find chords that are part of the key
     val chordsInKey = chords.map { chord =>
       if (baseKey.contains(chord)) DegreeInKey(chord.root - baseKey.root, baseKey)
       else UnknownDegree
     }
 
-    chordsInKey.foreach(println)
+    chordsInKey.map {
+      case UnknownDegree => "?"
+      case DegreeInKey(pitch, _) => tuning.printDegreeDescriptor(pitch)
+    }.foreach(println)
 
     chords.map { chord =>
       val degreePitch = chord.root - baseKey.root
