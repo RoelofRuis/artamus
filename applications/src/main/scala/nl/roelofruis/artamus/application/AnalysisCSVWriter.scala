27a8421cde45a934ed3302afda27ace7b635a8cf
Roelof Ruis <roelof.ruis@gmail.com>
2020-09-10 15:49:49 +0200

    Write with correct spacing in 4/4

--- applications/src/main/scala/nl/roelofruis/artamus/application/AnalysisCSVWriter.scala
+++ applications/src/main/scala/nl/roelofruis/artamus/application/AnalysisCSVWriter.scala
@@ -4,6 +4,7 @@ import java.io.{File, PrintWriter}
 
 import nl.roelofruis.artamus.application.Model.Settings
 import nl.roelofruis.artamus.core.analysis.rna.Model.RNAAnalysedChord
+import nl.roelofruis.artamus.core.primitives.Rational
 
 object AnalysisCSVWriter {
 
@@ -16,11 +17,18 @@ object AnalysisCSVWriter {
         case Some(analysis) =>
           val writer = new PrintWriter(new File(s"applications/charts/$filename.csv"))
           analysis.foreach { rnaNode =>
+            val duration = rnaNode.chord.window.duration
+            val timesFit = duration.v % Rational(1, 4)
+
+            // TODO: replace assumption of always fitting Rational(1, 4)
+            if (timesFit < 1) throw new NotImplementedError(s"Duration [$duration] does not fit")
+
             val chordName = settings.printChord(rnaNode.chord.element)
             val degree = settings.printDegree(rnaNode.degree)
             val absoluteKey = settings.printKeyDegree(rnaNode.absoluteKey)
             val relativeKey = settings.printKey(rnaNode.relativeKey)
             writer.write(s"$chordName;$degree;$absoluteKey;$relativeKey\n")
+            Range(0, timesFit - 1).foreach(_ => writer.write(";;;\n"))
           }
           writer.close()
       }
