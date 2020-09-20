f57931ae828a440bd62d1ddeced87431b7dce02d
RoelofRuis <roelof.ruis@gmail.com>
2020-09-20 22:30:30 +0200

    adding harmonic analyser outlines

---
+++ src/main/scala/nl/roelofruis/artamus/core/track/algorithms/harmonic/HarmonicAnalyser.scala
@@ -0,0 +1,13 @@
+package nl.roelofruis.artamus.core.track.algorithms.harmonic
+
+import nl.roelofruis.artamus.application.Model.Settings
+import nl.roelofruis.artamus.core.track.Pitched._
+import nl.roelofruis.artamus.core.track.algorithms.TunedMaths
+
+case class HarmonicAnalyser(settings: Settings) extends TunedMaths {
+
+  def analyiseHarmony(notes: NoteTrack): ChordTrack = {
+    ???
+  }
+
+}
