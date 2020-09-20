699ff28b479de4fa8a75ce9b381ded09ec72e697
RoelofRuis <roelof.ruis@gmail.com>
2020-09-20 19:54:16 +0200

    a naive voicer

--- src/main/scala/nl/roelofruis/artamus/core/track/transform/voicing/ChordVoicer.scala
+++ src/main/scala/nl/roelofruis/artamus/core/track/transform/voicing/ChordVoicer.scala
@@ -1,12 +1,21 @@
 package nl.roelofruis.artamus.core.track.transform.voicing
 
 import nl.roelofruis.artamus.application.Model.Settings
+import nl.roelofruis.artamus.core.common.Containers.Windowed
 import nl.roelofruis.artamus.core.track.Pitched._
+import nl.roelofruis.artamus.core.track.transform.TunedMaths
 
-case class ChordVoicer(settings: Settings) {
+case class ChordVoicer(settings: Settings) extends TunedMaths {
 
   def voiceChords(chordTrack: ChordTrack): NoteTrack = {
-    ???
+    chordTrack.map { case Windowed(window, element) =>
+      val noteGroup: NoteGroup = element
+        .quality
+        .intervals
+        .map(intervalDescriptor => Note(element.root + intervalDescriptor, 4))
+
+      Windowed(window, noteGroup)
+    }
   }
 
 }
