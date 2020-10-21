4875275cb0120fa86ad28924b57b00a0c6e7af57
Roelof Ruis <roelof.ruis@gmail.com>
2020-10-21 22:24:34 +0200

    added todo

--- src/main/scala/nl/roelofruis/artamus/core/track/Track.scala
+++ src/main/scala/nl/roelofruis/artamus/core/track/Track.scala
@@ -2,6 +2,7 @@ package nl.roelofruis.artamus.core.track
 
 import nl.roelofruis.artamus.core.track.Layer.{KeyChanges, MetreChanges}
 
+// TODO: move to layers namespace
 final case class Track(
   metres: MetreChanges,
   keys: KeyChanges,
